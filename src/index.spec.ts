8f3bf6c9aa7e0be274e1169467794e7b3dbc9b41
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-06-14 22:51:36 +0300

    test main logic

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,101 @@
-describe("Unit Testing!", () => {
-  it("should return true", () => {
-    expect(1 + 1).toEqual(2);
+import { buildInput } from "./utils";
+
+import { highestStableTower } from "./";
+
+describe("Main Logic", () => {
+  const input: Dimensions[] = [
+    {
+      length: 4,
+      width: 4,
+      height: 34
+    },
+    {
+      length: 5,
+      width: 3,
+      height: 50
+    },
+    {
+      length: 2,
+      width: 2,
+      height: 15
+    }
+  ];
+
+  const input1: Dimensions[] = [
+    {
+      length: 4,
+      width: 4,
+      height: 10
+    },
+    {
+      length: 2,
+      width: 2,
+      height: 15
+    }
+  ];
+  const input2: Dimensions[] = [
+    {
+      length: 3,
+      width: 1,
+      height: 10
+    },
+    {
+      length: 2,
+      width: 2,
+      height: 15
+    }
+  ];
+  const input3: Dimensions[] = [
+    {
+      length: 4,
+      width: 4,
+      height: 40
+    },
+    {
+      length: 5,
+      width: 5,
+      height: 50
+    },
+    {
+      length: 2,
+      width: 2,
+      height: 15
+    }
+  ];
+  const input4: Dimensions[] = [
+    {
+      length: 4,
+      width: 4,
+      height: 40
+    },
+    {
+      length: 5,
+      width: 5,
+      height: 50
+    },
+    {
+      length: 5,
+      width: 5,
+      height: 45
+    },
+    {
+      length: 2,
+      width: 2,
+      height: 15
+    }
+  ];
+
+  it.each<[Dimensions[], number]>([
+    [input1, 25],
+    [input2, 15],
+    [input3, 105]
+  ])("should return highest stable tower", (input, expected) => {
+    const result = highestStableTower(buildInput(input));
+    expect(result).toEqual(expected);
+  });
+
+  it.skip("should return highest stable tower with similar boxes", () => {
+    const result = highestStableTower(buildInput(input));
+    expect(result).toEqual(150);
   });
 });
